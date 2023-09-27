0fbbecb14fa8f0d771651fc79a27aa4636ea97cf
Shawn <975808127@qq.com>
2023-09-28 00:12:30 +0800

    初始化

---
+++ rust_greeting/greeting/target/debug/deps/greeting-61b4cb36b417a5b9.d
@@ -0,0 +1,5 @@
+C:\Users\YiSmallzier\Documents\vscode\rust_greeting\greeting\target\debug\deps\greeting-61b4cb36b417a5b9.rmeta: src\main.rs
+
+C:\Users\YiSmallzier\Documents\vscode\rust_greeting\greeting\target\debug\deps\greeting-61b4cb36b417a5b9.d: src\main.rs
+
+src\main.rs:
---
+++ rust_greeting/greeting/target/debug/deps/greeting-ffdfeb43effe644e.d
@@ -0,0 +1,5 @@
+C:\Users\YiSmallzier\Documents\vscode\rust_greeting\greeting\target\debug\deps\greeting-ffdfeb43effe644e.rmeta: src\main.rs
+
+C:\Users\YiSmallzier\Documents\vscode\rust_greeting\greeting\target\debug\deps\greeting-ffdfeb43effe644e.d: src\main.rs
+
+src\main.rs:
